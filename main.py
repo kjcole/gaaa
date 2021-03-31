3680e8b0a6fd9d65ef01977b1ddfb0e6f96573f7
Kevin Cole <dc.loco@gmail.com>
2021-03-31 17:15:03 -0400

    Fight with Javascript to get dictionary working

--- main.py
+++ main.py
@@ -12,23 +12,26 @@ tonic = 27.5
 twelfth_root = 1.0594630943592953
 note = tonic
 scale = []
-keyboard = {"z": Note.C4,
-            "s": Note.CSHARP4,
-            "x": Note.D4,
-            "d": Note.EB4,
-            "c": Note.E4,
-            "v": Note.F4,
-            "g": Note.FSHARP4,
-            "b": Note.G4,
-            "h": Note.GSHARP4,
-            "n": Note.A4,
-            "j": Note.BB4,
-            "m": Note.B4,
-            ",": Note.C5,
-            "l": Note.CSHARP5,
-            ".": Note.D5,
-            ";": Note.EB5,
-            "/": Note.E5}
+# unknown expression:  178
+keyboard = {
+    "z" : Note.C4,
+    "s" : Note.CSharp4,
+    "x" : Note.D4,
+    "d" : Note.Eb4,
+    "c" : Note.E4,
+    "v" : Note.F4,
+    "g" : Note.FSharp4,
+    "b" : Note.G4,
+    "h" : Note.GSharp4,
+    "n" : Note.A4,
+    "j" : Note.Bb4,
+    "m" : Note.B4,
+    "," : Note.C5,
+    "l" : Note.CSharp5,
+    "." : Note.D5,
+    ";" : Note.Eb5,
+    "/" : Note.E5,
+}
 for index in range(88):
     scale.append(note)
     note = note * twelfth_root
@@ -38,9 +41,9 @@ for index in range(88):
 
 def on_forever():
     global ping
-    global scale
-    global keyboard
     ping = serial.read_buffer(0)
     if len(ping) > 0:
-        basic.show_string("" + (ping.to_string()))
+        ivory = ping.to_string()
+        music.play_tone(keyboard[ivory], music.beat(BeatFraction.EIGHTH))
+        basic.show_string("" + ping.to_string())
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,19 +1,16 @@
-/** 
-
-Written by Kevin Cole <ubuntourist@hacdc.org> 2021.03.25
-
-Use screen or putty to generate "music" (and I use the term "music" loosely)
-
-
+/**
+ * Written by Kevin Cole <ubuntourist@hacdc.org> 2021.03.25
+ * 
+ * Use screen or putty to generate "music" (and I use the term "music" loosely)
  */
+let ivory = 0
 let ping : Buffer = null
 serial.redirectToUSB()
 let tonic = 27.5
-//  2.0 ** (1.0 / 12.0)
+// 2.0 ** (1.0 / 12.0)
 let twelfth_root = 1.0594630943592953
 let note = tonic
-let scale = []
-let keyboard = {
+let keyboard : any = {
     "z" : Note.C4,
     "s" : Note.CSharp4,
     "x" : Note.D4,
@@ -32,21 +29,19 @@ let keyboard = {
     ";" : Note.Eb5,
     "/" : Note.E5,
 }
-
 for (let index = 0; index < 88; index++) {
+    let scale: number[] = []
     scale.push(note)
     note = note * twelfth_root
 }
-//  basic.pause(200)
-//  music.play_tone(tonic, music.beat(BeatFraction.WHOLE))
-//  print(tonic)
-basic.forever(function on_forever() {
-    
-    
-    
+// basic.pause(200)
+// music.play_tone(tonic, music.beat(BeatFraction.WHOLE))
+// print(tonic)
+basic.forever(function () {
     ping = serial.readBuffer(0)
     if (ping.length > 0) {
-        basic.showString("" + ping.toString())
+        ivory = keyboard[ping.toString()]
+        music.playTone(ivory, music.beat(BeatFraction.Half))
+        basic.showString("" + (ping.toString()))
     }
-    
 })
